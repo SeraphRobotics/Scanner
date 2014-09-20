3134101338d3efe3827f3af1aae89d0265d28a7d
Jeffrey Lipton <jeffreyilipton@gmail.com>
2014-09-19 20:44:01 -0400

    working on Windows

--- Raspberry-pi/ScannerController.py
+++ Raspberry-pi/ScannerController.py
@@ -1,58 +1,68 @@
-import PySide
-import ScannerInterface
+from PySide.QtCore import *
+from ScannerInterface import *
 import cv2
 
 
 class ScannerController(QObject):
-	errored = Signal()
+    
+    errored = Signal()
 
-	def __init__(self):
-		self.interface = ScannerInterface()
-		self.interface.connect("COM10",9600)
-		self.capwebcam = cv2.VideoCapture(0)
-		self.width_ = 1920
-		self.height_= 1080
-		self.timer = PySide.QtCore.QTimer();
-		self.framerate = 5
-		self.timer.setInterval(1000.00/self.framrate)
-		
-		
-		
-		self.time = 0.0
-		self.maxtime = 30.0*1000.0
-		self.speed = 10.0 ##mm/s
-		self.imformat = ".jpeg"
-		
-		errored.connect(self.interface.error)
-		self.timer.timeout.connect(ontimeout)
-		self.__setupCamera()
-		
-	def __setupCamera(self):
-		
-		if not self.capwebcam.isOpened():
-			self.capwebcam.open()
-		if not self.capwebcam.isOpened():
-			self.errored.emit()
-		
-		state=true;
-		state = state && capwebcam.set(CV_CAP_PROP_FRAME_WIDTH,self.width_);
-		state = state && capwebcam.set(CV_CAP_PROP_FRAME_HEIGHT,self.height_);
-		if(not state):
-			self.errored.emit()
-	
-	@Slot()
-	def buttonPressed(self):
-		self.interface.startscan()
-		self.timer.start()
-	
-	def ontimeout(self):
-		self.time = self.time + 1000.00/self.framrate
-		dist = self.speed*self.time/1000.0
-		ret, frame = cap.read()
-		imwrite('%s'%dist+self.imformat,frame)
-		
-		if self.time>self.maxtime:
-			self.timer.stop()
-			self.time = 0
-			self.interface.home()
-		
+    def __init__(self):
+        super(ScannerController, self).__init__()
+        self.interface = ScannerInterface()
+        self.interface.connectport("COM6",9600)
+        self.capwebcam = cv2.VideoCapture(0)
+        self.width_ = 1920
+        self.height_= 1080
+        self.timer = QTimer();
+        self.framerate = 5
+        self.timer.setInterval(1000.00/self.framerate)
+        
+        self.time = 0.0
+        self.maxtime = 30.0*1000.0
+        self.speed = 10.0 ##mm/s
+        self.imformat = ".jpeg"
+        
+        
+        self.timer.timeout.connect(self.ontimeout)
+        
+        self.__setupCamera()
+        
+    def __setupCamera(self):
+        
+        if not self.capwebcam.isOpened():
+            self.capwebcam.open()
+            print "camera didnt open"
+        if not self.capwebcam.isOpened():
+            self.errored.emit()
+            print "camera didnt open twice"
+        
+        state=True;
+        state = state and self.capwebcam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,self.width_);
+        state = state and self.capwebcam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,self.height_);
+        if(not state):
+            self.errored.emit()
+            print "camera didnt get settings"
+    
+    @Slot()
+    def buttonPressed(self):
+        self.interface.startscan()
+        self.timer.start()
+    
+    def ontimeout(self):
+        self.time = self.time + 1000.00/self.framerate
+        dist = self.speed*self.time/1000.0
+        ret, frame = self.capwebcam.read()
+        cv2.imwrite('%s'%dist+self.imformat,frame)
+        if self.time>self.maxtime:
+            self.timer.stop()
+            self.time = 0
+            self.interface.home()
+
+if __name__ == '__main__':
+    import sys
+    app = QCoreApplication(sys.argv)
+    sc = ScannerController()
+	QTimer.singleShot(1000,sc.buttonPressed)
+    sc.errored.connect(sc.interface.error)
+    sys.exit(app.exec_())
--- Raspberry-pi/ScannerInterface.py
+++ Raspberry-pi/ScannerInterface.py
@@ -1,22 +1,23 @@
 import serial
-import PySide
+from PySide.QtCore import *
 
 class ScannerInterface(QObject):
     
-	pressed = Signal()
-	scanCompleted = Signal()
-	errored = Signal()
-	
-	def __init__(self):
-        self.ser = serial.Serial()
-		self.timer = PySide.QtCore.QTimer();
-		self.timer.setInterval(100)
+    pressed = Signal()
+    scanCompleted = Signal()
+    errored = Signal()
     
-	def __del__(self):
+    def __init__(self):
+        super(ScannerInterface, self).__init__()
+        self.ser = serial.Serial()
+        self.timer = QTimer();
+        self.timer.setInterval(100)
+    
+    def __del__(self):
         self.ser.close()
     
-	@Slot()
-	def connect(self,port,baud):
+    @Slot()
+    def connectport(self,port,baud):
         if (self.ser.isOpen()): self.ser.close()
         self.ser.setPort(port)
         self.ser.baudrate = baud
@@ -24,58 +25,58 @@ class ScannerInterface(QObject):
     def isready(self):
         return self.ser.isOpen()
     
-	def _write(self,towrite):
+    def _write(self,towrite):
         if (not self.isready()):
             return False
         else:
             self.ser.write(towrite)
         return True
-	
-	@Slot()
-	def _checkBuffer(self):
-		if(self.isready()):
-			while(self.ser.inWaiting()>0):
-				char = self.ser.read();
-				if char == "B":
-					self.pressed.emit();
-				elif char =="D":
-					self.scanCompleted.emit();
-				elif char =="E":
-					self.errored.emit();
-	
-	@Slot()
-	def error(self):
-		self._write("e")
-	
-	@Slot()
-	def startscan(self):
-		self._write("s")
     
-	@Slot()
-	def laserOn(self):
-		self._write("l")
-	
-	@Slot()
-	def laserOff(self):
-		self._write("k")
-	
-	@Slot()
-	def ledOn(self):
-		self._write("o")
-	
-	@Slot()
-	def ledOff(self):
-		self._write("f")
-	
-	@Slot()
-	def home(self):
-		self._write("h")
-	
+    @Slot()
+    def _checkBuffer(self):
+        if(self.isready()):
+            while(self.ser.inWaiting()>0):
+                char = self.ser.read();
+                if char == "B":
+                    self.pressed.emit();
+                elif char =="D":
+                    self.scanCompleted.emit();
+                elif char =="E":
+                    self.errored.emit();
+    
+    @Slot()
+    def error(self):
+        self._write("e")
+    
+    @Slot()
+    def startscan(self):
+        self._write("s")
+    
+    @Slot()
+    def laserOn(self):
+        self._write("l")
+    
+    @Slot()
+    def laserOff(self):
+        self._write("k")
+    
+    @Slot()
+    def ledOn(self):
+        self._write("o")
+    
+    @Slot()
+    def ledOff(self):
+        self._write("f")
+    
+    @Slot()
+    def home(self):
+        self._write("h")
+    
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
+    app = QApplication(sys.argv)
     ir = ScannerInterface()
-    ir.connect("COM10",9600)
+    ir.connectport("COM6",9600)
     print ir.isready()
     loopVar=True
     while(loopVar):
